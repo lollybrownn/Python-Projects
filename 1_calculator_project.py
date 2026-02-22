45dd6ef4673e113983e559c1cfdbf2602901d3fa
Albert <albertusbagus.dya@gmail.com>
2026-02-23 00:33:27 +0700

    Add exit option and print results per case

--- 1_calculator_project.py
+++ 1_calculator_project.py
@@ -1,11 +1,13 @@
 # This is a Simple Calculator
 # That can calculate Addition, Subtraction, Multiplication, and Division
 
+
 print("==== SIMPLE CALCULATOR ====")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
+print("5. Exit Program")
 
 choice = int(input("Select the calculation: "))
 
@@ -15,13 +17,19 @@ num2 = int(input("Input 2nd number: "))
 match choice:
     case 1:
         total = num1 + num2
+        print(f"\nTotal Calculation: {total}")
     case 2:
         total = num1 - num2
+        print(f"\nTotal Calculation: {total}")
     case 3:
         total = num1 * num2
+        print(f"\nTotal Calculation: {total}")
     case 4:
         total = num1 / num2
+        print(f"\nTotal Calculation: {total}")
+    case 5:
+        exit(0)
     case _:
         total = "\nInvalid input, try again"
 
-print(f"\nTotal Calculation: {total}")
+
