cd5b67f263eb04b6fd2179de7f80f4ad6bd53eed
Albert <albertusbagus.dya@gmail.com>
2026-02-22 21:48:06 +0700

    made the ui for the calculator

--- 1_calculator_project.py
+++ 1_calculator_project.py
@@ -1,3 +1,10 @@
 # This is a Simple Calculator
 # That can calculate Addition, Subtraction, Multiplication, and Division
 
+print("==== SIMPLE CALCULATOR ====")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = int(input("Select the calculation : "))
