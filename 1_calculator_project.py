9329c3775b92ad96be989281e16b230c2eb37ead
Albert <albertusbagus.dya@gmail.com>
2026-02-22 21:59:45 +0700

    Complete the rough logic of the calculator

--- 1_calculator_project.py
+++ 1_calculator_project.py
@@ -7,4 +7,21 @@ print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
 
-choice = int(input("Select the calculation : "))
+choice = int(input("Select the calculation: "))
+
+num1 = int(input("\nInput 1st number: "))
+num2 = int(input("Input 2nd number: "))
+
+match choice:
+    case 1:
+        total = num1 + num2
+    case 2:
+        total = num1 - num2
+    case 3:
+        total = num1 * num2
+    case 4:
+        total = num1 / num2
+    case _:
+        total = "\nInvalid input, try again"
+
+print(f"\nTotal Calculation: {total}")
